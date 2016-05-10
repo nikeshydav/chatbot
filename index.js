f3ca50fea2f2a0acb65f1fb624ce367e746627b8
nikeshydav <nikesh.yadav@gmail.com>
2016-05-11 00:57:27 +0530

    init

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-'use strict'
-const http = require('http')
-const Bot = require('messenger-bot')
+var http = require('http');
 
-let bot = new Bot({
-  token: 'PAGE_TOKEN',
-  verify: 'VERIFY_TOKEN'
-})
-
-bot.on('error', (err) => {
-  console.log(err.message)
-})
-
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text
-
-  bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
-
-    reply({ text }, (err) => {
-      if (err) throw err
-
-      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
-  })
-})
-
-http.createServer(bot.middleware()).listen(3000)
+http.createServer(function (request,response){
+   
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.write("Adsfasdf");
+    response.end();
+}).listen(8888);
